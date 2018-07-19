035ab693994cdbb628041719bcae083e741548f0
vikasgautam <vikas.gautam18@gmail.com>
2018-07-19 21:29:48 +0200

    movie names in place of movie id in the output

---
+++ readme.md
@@ -0,0 +1,6 @@
+Analysis on movie lens dataset with Apache Spark in Scala
+
+1. Sort movies by popularity
+2. Find movies with lowest and highest average ratings
+3. Sort movies by average ratings
+
--- src/main/scala/com/wordpress/technicado/movielens/SortMoviesByPopularity.scala
+++ src/main/scala/com/wordpress/technicado/movielens/SortMoviesByPopularity.scala
@@ -4,29 +4,36 @@ import org.apache.spark.sql.{DataFrame, Dataset, SaveMode, SparkSession}
 
 object SortMoviesByPopularity {
   def main(args: Array[String]): Unit = {
-    if(args.length != 2){
+
+    if(args.length != 3){
       println("USAGE: spark-submit --class com.wordpress.technicado.movielens.SortMoviesByPopularity " +
         "--master local[*] jars/movielens_analysis_2.11-0.1.jar " +
-        "mapreduceexample/movielens/input/u.data mapreduceexample/movielens/output1")
+        "/datasets/movielens/ml-100k/u.data /datasets/movielens/ml-100k/u.item spark-examples/SortMoviesByPopularity/output")
       System.exit(-1)
     }
 
     val spark = SparkSession.builder().appName("SortMoviesByPopularity")
       .getOrCreate()
-    import spark.implicits._
-    val inputDS: Dataset[MovieRating] = spark.read.textFile(args(0)).map(_.split("\t"))
-      .map(arr => MovieRating(arr(0), arr(1), arr(2), arr(3)))
 
-    inputDS.toDF().createOrReplaceTempView("MovieRatings")
-    val sortedDF = spark.sql("select movieId, count(1) from MovieRatings group by movieId order by count(1) desc")
+    val mRatings = spark.read.option("delimiter", "\t").csv(args(0))
+      .toDF("user_id", "movie_id", "rating", "timestamp")
+      .select("user_id", "movie_id", "rating")
+
+
+    val mItem = spark.read.option("delimiter", "|").csv(args(1))
+      .select("_c0", "_c1")
+      .toDF("movie_id", "movie_name")
+
+
+    val uData = mRatings.join(mItem, "movie_id").select("movie_name", "rating")
+    uData.toDF().createOrReplaceTempView("MovieRatings")
+    val sortedDF = spark.sql("select movie_name, count(1) from MovieRatings group by movie_name order by count(1) desc")
 
     val tsvWithHeaderOptions: Map[String, String] = Map(
       ("delimiter", "\t"), // Uses "\t" delimiter instead of default ","
       ("header", "true"))  // Writes a header record with column names
 
     sortedDF.coalesce(1).write.mode(SaveMode.Overwrite)
-      .options(tsvWithHeaderOptions).csv(args(1))
+      .options(tsvWithHeaderOptions).csv(args(2))
   }
 }
-
-case class MovieRating(userId: String, movieId: String, rating: String, timestamp: String)
