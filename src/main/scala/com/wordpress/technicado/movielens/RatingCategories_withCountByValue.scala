1ebf6c99360daf24c3ce5effcd73f284f86fd669
vikasgautam <vikas.gautam18@gmail.com>
2018-07-20 16:31:26 +0200

    LowestAndHighestAverageRating

--- src/main/scala/com/wordpress/technicado/movielens/RatingCategories_withCountByValue.scala
+++ src/main/scala/com/wordpress/technicado/movielens/RatingCategories_withCountByValue.scala
@@ -1,41 +1,34 @@
 package com.wordpress.technicado.movielens
 
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.rdd.RDD
 
-/**
-  *
-  * RDD based approach to find the number of ratings per rating category
-  *
-  */
-object RatingCategories {
+object RatingCategories_withCountByValue {
 
   def main(args: Array[String]): Unit = {
 
     Logger.getLogger("org").setLevel(Level.ERROR)
 
-    if(args.length != 2){
-      println("USAGE: spark-submit --class com.wordpress.technicado.movielens.RatingCategories " +
+    if(args.length != 0){
+      println("USAGE: spark-submit --class com.wordpress.technicado.movielens.RatingCategories_withCountByValue " +
         "--master local[*] jars/movielens_analysis_2.11-0.1.jar ")
       System.exit(-1)
     }
 
     Utils.readConfig("conf/movielens.properties")
-
     val sparkConf = new SparkConf().setAppName("RatingCategories")
     val sc = new SparkContext(sparkConf)
 
     val uData: RDD[String] = sc.textFile(Utils.getString("movielens.rating.udata"))
 
-    val y: RDD[(String, Int)] = uData.map(row => {
-      val arr = row.split("\t")
-      (arr(2), 1)
+    val ratingRDD = uData.map(f => {
+      f.split("\t")(2)
     })
 
-    val x: RDD[(String, Int)] = y.reduceByKey(_ + _)
+    val ratingHistogram: collection.Map[String, Long] = ratingRDD.countByValue()
 
-    x.foreach(r => println(r._1 + " --> " + r._2))
-
+    println("======== The ratings can be categorized as below =========")
+    ratingHistogram.toSeq.foreach(println)
   }
 }
